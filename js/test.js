9ef0b009ca37d8748b24bf53822baf171a2969fc
CRG9 <chris.galetta@gmail.com>
2025-09-27 20:05:39 -0400

    Revert "implementing a diagnostics test to determine boss-models script bug"
    This reverts commit 0adcff29103652d5adcb363a88392a5f22b1f5bd.

--- js/test.js
+++
@@ -1,6 +0,0 @@
-// This import path is the second part of the test
-import * as THREE from '../libs/three.module.js';
-
-// If the browser can execute this code, the import was successful.
-console.log('SUCCESS: three.module.js was loaded!', THREE);
-document.getElementById('status').textContent = 'SUCCESS! The path to the libs folder is correct.';
